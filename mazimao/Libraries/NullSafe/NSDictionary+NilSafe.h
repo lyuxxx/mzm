cbef75c2e6ffee7204904b86962f0fea44db9a3f
lyx <lyuxxx@163.com>
2019-04-19 17:41:25 +0800

    NSDictionary+NilSafe

---
+++ mazimao/Libraries/NullSafe/NSDictionary+NilSafe.h
@@ -0,0 +1,17 @@
+//
+//  NSDictionary+NilSafe.h
+//  dssp
+//
+//  Created by yxliu on 2018/1/2.
+//  Copyright © 2018年 capsa. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSDictionary (NilSafe)
+
+@end
+
+@interface NSMutableDictionary (NilSafe)
+
+@end
